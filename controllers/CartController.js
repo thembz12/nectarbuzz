f909907272fc1d29dba61d63b7a849cb166747af
thembz12 <oyin1239@gmail.com>
2024-09-12 07:03:44 +0100

    update

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,5 +1,5 @@
 const CartModel = require('../models/Cart');
-const ProductModel = require('../models/Product');
+const ProductModel = require('../models/Product'); 
 
 // Add product to cart
 exports.addToCart = async (req, res) => {
