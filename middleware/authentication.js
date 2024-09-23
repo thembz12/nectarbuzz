11ccd0fc5ab9f1f7ed84b4b4144c994633389383
thembz12 <oyin1239@gmail.com>
2024-09-23 09:44:31 +0100

    update 2

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -111,7 +111,7 @@ const isAdmin = async (req, res, next) => {
         return res.status(401).json({ message: 'Action requires sign-in. Please log in to continue.' });
       }
   
-      const decodedToken = jwt.verify(token, process.env.jwt_secret);
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
       const user = await UserModel.findById(decodedToken.userId);
       if (!user) {
         return res.status(404).json({ message: 'Authentication Failed: User not found' });
